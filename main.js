eefb3098de77d7a044f22db7a34f2aa07ee3db83
Alex Barry <albarrync@gmail.com>
2020-03-07 12:16:51 -0500

    Add form for Palindrome testing

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let Phrase = require("albarrync-palindrome");
 
-let string = prompt("Please enter a string for Palindrome testing:");
+function palindromeTester(event) {
+  event.preventDefault();
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `<b>"${phrase.content}"</b> is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `<b>"${phrase.content}"</b> is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
